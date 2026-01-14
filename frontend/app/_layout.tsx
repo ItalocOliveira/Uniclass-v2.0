94b5bbc7993aa15163cb588da4bfec5965b2cd17
Marlon Mena <141514595+Marlon-Mena@users.noreply.github.com>
2026-01-13 21:02:00 -0300

    ajustando

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,7 +3,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
