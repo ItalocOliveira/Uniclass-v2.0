27008294faea28ca969dde400e16a68cd3fad1e7
Marlon Mena <141514595+Marlon-Mena@users.noreply.github.com>
2026-01-14 09:33:22 -0300

    nativeWind funcionando 100%

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -14,7 +14,7 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontStyle: 'italic', fontSize: 20 }}>marlon</Text>
+    <View className='flex-1 justifyContent bg-slate-400'>
+      <Text>marlon</Text>
     </View>);
 }
