7fa66ee5d83040d86c91934389cb9e21cb4914c9
Marlon Mena <141514595+Marlon-Mena@users.noreply.github.com>
2026-01-23 10:47:25 -0300

    Mudando a aparencia da tela Home

--- frontend/app/Home/index.tsx
+++ frontend/app/Home/index.tsx
@@ -17,8 +17,8 @@ import Carousel from "react-native-reanimated-carousel";
 
 const catalogo = [
     { id: 1, title: "Fábrica de Software", image: require("../image/Captura de tela 2026-01-20 110141.png") },
-    { id: 2, title: "Evento Tuor", image: require("../image/Captura de tela 2026-01-20 110232.png") },
-    { id: 3, title: "Oi", image: require("../image/oi.png") },
+    { id: 2, title: "Evento de Autista", image: require("../image/Captura de tela 2026-01-20 110232.png") },
+    { id: 3, title: "Evento Tuor", image: require("../image/oi.png") },
 ]
 
 export default function Home() {
@@ -36,28 +36,30 @@ export default function Home() {
             <View style={styles.gap} />
 
             <View style={styles.navBar}>
-                <TouchableOpacity style={styles.chevron}>
-                    <IconChevronLeft stroke={1.5} color="#999" />
-                </TouchableOpacity>
-
-                <View style={styles.container}>
-                    <Text style={styles.header}>Eventos</Text>
-                    <Carousel loop width={320} height={220} autoPlay={true} autoPlayInterval={3000} data={catalogo} renderItem={({ item }) => (
+                <View >
+                    <Text style={[styles.header, styles.fontAnta]}>Eventos</Text>
+                    <Text style={[styles.bodyText, styles.fontAnta]}>Todos os Eventos mais Recente</Text>
+                    <br />
+                    <Carousel loop width={390} height={220} autoPlay={true} autoPlayInterval={6000} data={catalogo} renderItem={({ item }) => (
                         <View style={styles.card}>
                             <Image source={item.image} style={styles.image} />
-                            <Text style={styles.text}>{item.title}</Text>
+                            <Text style={[styles.text, styles.fontAnta]}>{item.title}</Text>
                         </View>
                     )}
                     />
                 </View>
 
-                <TouchableOpacity style={styles.chevron}>
-                    <IconChevronRight stroke={1.5} color="#999" />
-                </TouchableOpacity>
             </View>
 
-            <View style={styles.section}>
-                <Text style={styles.sectionText}>Teste</Text>
+            <View style={styles.gap}></View>
+
+            <View style={styles.maps}>
+                <View>
+                    <Text style={[styles.header, styles.fontAnta]}>Mapa do Campus</Text>
+                    <Text style={[styles.bodyText, styles.fontAnta]}>Explore o Mapa do Campus Abaixo</Text>
+                    <br />
+                    
+                </View>
             </View>
         </ScrollView>
     );
@@ -67,6 +69,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#fff",
+    },
+    maps: {
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        marginBottom: 20,
+        borderRadius: 8,
     },
     titulo: {
         flexDirection: "row",
@@ -80,6 +89,9 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         textAlign: "center",
         marginBottom: 20,
+    },
+    bodyText: {
+
     },
     card: {
         flex: 1, borderRadius: 10,
@@ -105,14 +117,14 @@ const styles = StyleSheet.create({
         fontSize: 20,
     },
     gap: {
-        height: 20,
+        height: 40,
     },
     navBar: {
+        backgroundColor: "#ddd",
         flexDirection: "row",
         justifyContent: "space-between",
         paddingHorizontal: 20,
         paddingVertical: 10,
-        backgroundColor: "#f0f0f0",
         marginBottom: 20,
         borderRadius: 8,
     },
