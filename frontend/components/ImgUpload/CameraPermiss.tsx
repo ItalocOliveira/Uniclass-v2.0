2ba3ba977369e6492272de40cfb3bdbb3b90413b
Rafaella <rafaelladiniz309@gmail.com>
2026-01-24 11:30:13 -0300

    tela login

--- frontend/components/ImgUpload/CameraPermiss.tsx
+++ frontend/components/ImgUpload/CameraPermiss.tsx
@@ -1,48 +1,35 @@
 import { useState, useEffect } from "react";
-import { Camera, CameraType  } from "expo-camera";
-import { View, Text, StyleSheet} from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import { View, Text, StyleSheet } from "react-native";
 
-
-export function Camerapermiss(){
-
-  const [camera, setCamera] = useState<CameraType>('back');
+export function Camerapermiss() {
+  const [camera, setCamera] = useState<CameraType>("back");
   const [permissao, setPermissao] = useState<boolean | null>(null);
 
   useEffect(() => {
-    (async ()=> {
-      const {status} = await Camera.requestCameraPermissionsAsync();
-      setPermissao(status === "granted")
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setPermissao(status === "granted");
+    })();
+  }, []);
 
-  })();
+  if (permissao === null) {
+    return <View />;
+  }
 
-  },[]);
-
-
-
-
-  if (permissao === null){
-    return(
-      <View/>
+  if (permissao === false) {
+    return (
+      <Text style={estilizar.textoPermissao}>
+        "É necessário permissão da câmera!"
+      </Text>
     );
-   
   }
-
-  if (permissao === false){
-    return(
-      <Text style={estilizar.textoPermissao}>"É necessário permissão da câmera!"</Text>
-    )
-  }
-
-  
-
 }
 
-
 const estilizar = StyleSheet.create({
-  textoPermissao:{
-    color:"red",
-    textAlign:"center",
-    padding:20,
+  textoPermissao: {
+    color: "red",
+    textAlign: "center",
+    padding: 20,
   },
-
-})
+});
