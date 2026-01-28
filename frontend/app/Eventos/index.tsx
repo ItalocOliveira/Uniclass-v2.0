4c3681f3181c046cb1547aa2651caaeb8278d593
Josevictor125 <jose7.victor7t@gmail.com>
2026-01-28 13:44:11 -0300

    evento

---
+++ frontend/app/Eventos/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, ScrollView, StyleSheet } from "react-native"
+
+export default function Evento(){
+    return(
+        <ScrollView>
+            <View>
+                
+            </View>
+        </ScrollView>
+    )
+};
