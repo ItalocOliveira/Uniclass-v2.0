887e8e8cd12bd5ffc1b10e04f4c6f600f4c6e169
Marlon Mena <141514595+Marlon-Mena@users.noreply.github.com>
2026-02-05 21:15:50 -0300

    adicionando o fetcher de login

--- frontend/app/Login/index.tsx
+++ frontend/app/Login/index.tsx
@@ -10,6 +10,7 @@ import {
 import { styles } from "./styles";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
@@ -19,14 +20,44 @@ export default function LoginScreen() {
   async function getLogin() {
     try {
       setLoading(true);
+
       if (!email || !password) {
-        return Alert.alert("Anteção", "Informe os campos obrigatórios!");
+        return Alert.alert("Atenção", "Informe os campos obrigatórios!");
       }
-      setTimeout(() => {
-        Alert.alert("Logado com sucesso");
-      }, 2000);
+
+      const response = await fetch("localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          senha: password,
+          instituicaoId: "550e8400-e29b-41d4-a716-446655440000"
+        }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        return Alert.alert("Erro", data.message || "Credenciais inválidas");
+      }
+
+      const token = data.access_token;
+
+      const usuario = data.usuario;
+
+      await AsyncStorage.setItem("token", token);
+      await AsyncStorage.setItem("usuario", JSON.stringify(usuario));
+
+      Alert.alert("Login realizado com sucesso!");
+
+      console.log("TOKEN:", token);
+      console.log("USUÁRIO:", usuario);
+
     } catch (error) {
       console.log(error);
+      Alert.alert("Erro", "Falha ao conectar com o servidor");
     } finally {
       setLoading(false);
     }
@@ -38,13 +69,17 @@ export default function LoginScreen() {
         source={require("../image/logo_uniclass.png")}
         style={styles.image}
       />
+
       <View>
         <View style={styles.boxInput}>
           <TextInput
             placeholder="E-MAIL INSTITUCIONAL"
             placeholderTextColor="#999"
             style={styles.input}
+            value={email}
             onChangeText={setEmail}
+            autoCapitalize="none"
+            keyboardType="email-address"
           />
           <MaterialIcons name="email" size={30} color="gray" />
         </View>
@@ -65,7 +100,7 @@ export default function LoginScreen() {
       </View>
 
       <View style={styles.boxButton}>
-        <TouchableOpacity style={styles.button} onPress={() => getLogin()}>
+        <TouchableOpacity style={styles.button} onPress={getLogin}>
           {loading ? (
             <ActivityIndicator color={"white"} size={"small"} />
           ) : (
